091c85cf0da159817f956f00bdb691bc329b372d
simon freeman <spfreeman@live.co.uk>
2018-04-16 22:29:30 +0100

    starting work on viewing all tech resources view and setting up initial view so that it can swap view components later

--- src/components/initial-view.vue
+++ src/components/initial-view.vue
@@ -1,10 +1,47 @@
 <template>
- <p>Initial view </p>
+    <component v-bind:is="currentViewComponent.component">fff</component>
 </template>
 
 <script>
+import ViewAllSources from '@/components/view-all-sources';
+
+const fakeSources = [
+  {
+    name: 'uncanny county',
+    url: 'https://audioboom.com/channels/4741997.rss',
+  },
+  {
+    name: 'uncanny county',
+    url: 'https://rss.acast.com/thetipoff',
+  },
+];
+
+const sourceViewComponents = [
+  {
+    name: 'viewAll',
+    component: {
+      template: '<view-all-sources :sources="fakeSources" />',
+      components: {
+        ViewAllSources,
+      },
+      data() {
+        return {
+          fakeSources,
+        };
+      },
+    },
+  },
+];
+
+
 export default {
   name: 'intial-view',
+  data() {
+    return {
+      sourceViewComponents,
+      currentViewComponent: sourceViewComponents[0],
+    };
+  },
 };
 </script>
 
